04673d6471a83e1db04bd404192704bf86793f4e
joshuaboddy <58597390+joshuaboddy@users.noreply.github.com>
2021-11-17 20:12:40 +0000

    gross day 3

--- 2019/day3/soln.jl
+++ 2019/day3/soln.jl
@@ -16,31 +16,37 @@ function make_all_moves(path)
 
     start = [0,0]
     coords_hit = [start]
+    steps = [0]
     moves = split(path, ",")
     for move in moves
-        coords_hit = [coords_hit; make_move(coords_hit[end], move)]
+        coords_hit = vcat(coords_hit, make_move(coords_hit[end], move))
+        steps = [steps; [1 for n=1:parse(Int64, (move[2:end]))]]
     end
 
-    return Set(coords_hit)
+    return coords_hit, steps
 
 end
 
 
-function part1()
+function both_parts()
 
     data = readlines(path * "\\input.txt")
 
-    coords = []
+    coords = [[]]
+    steps = [[]]
 
     for path in data
-        coords = [coords; make_all_moves(path)]
+        coords_in_path, steps_in_path = make_all_moves(path)
+        coords = hcat(coords, [coords_in_path])
+        steps = hcat(steps, [steps_in_path])
     end
 
-    crossings = intersect(coords[1], coords[2])
+    crossings = intersect(coords[2], coords[3])
     manhattan = minimum([sum(abs.(x)) for x in crossings if x != [0,0]])
-
-    return manhattan
+    min_steps_in_crossing = minimum([accumulate(+, steps[2])[findfirst(isequal(c), coords[2])] + accumulate(+, steps[3])[findfirst(isequal(c), coords[3])] for c in crossings if c != [0,0]])
+    
+    return manhattan, min_steps_in_crossing
 
 end
 
-print(part1())
+print(both_parts())
