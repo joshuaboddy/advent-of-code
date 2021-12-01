af3f50833895166f7a98d8b3708989397fb45e90
joshuaboddy <58597390+joshuaboddy@users.noreply.github.com>
2021-12-01 21:47:01 +0000

    updated for nicer syntax

--- 2021/day 1/soln.jl
+++ 2021/day 1/soln.jl
@@ -3,14 +3,14 @@ data = parse.(Int, readlines(path * "\\input.txt"))
 
 function part1()
 
-    sum([1 for x in diff(data) if x > 0])
+    return sum(diff(data) .> 0)
 
 end
 
 function part2()
 
     threes = [sum(data[1+i:3+i]) for i ∈ 0:length(data)-3]
-    sum([1 for x in diff(threes) if x > 0])
+    return sum(diff(threes) .> 0)
 
 end
 
