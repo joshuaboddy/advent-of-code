6c1064a5afa8e0cba1eb6861a7e294d0624994f7
joshuaboddy <58597390+joshuaboddy@users.noreply.github.com>
2021-12-04 13:30:36 +0000

    day 4
    RIP whirlwind romance with Julia

---
+++ 2021/day 4/soln.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+
+def bingo(df):
+    
+    marked = df == 'x'
+    
+    column_winner = (marked.sum(axis=0) == len(df)).any()
+    row_winner = (marked.sum(axis=1) == len(df)).any()
+    diag_winner = False #(np.diag(marked).sum() == len(df)) | (np.diag(np.fliplr(marked)).sum() == len(df))
+
+    return (column_winner | row_winner | diag_winner)
+
+
+def parse():
+    
+    f = open('input.txt')
+    raw = f.readlines()
+    f.close()
+    
+    lines = list(raw)
+    lines = lines + ['\n']
+    
+    numbers_drawn = [int(x) for x in lines[0].strip().split(',')]
+    
+    bingo_cards = []
+    current_card = pd.DataFrame()
+    
+    for line in lines[2:]:
+        
+        line = line.strip()
+        
+        if line != '':
+            current_card = current_card.append(pd.Series([int(x) for x in line.split()]),
+                                               ignore_index=True)
+        else:
+            bingo_cards.extend([current_card])
+            current_card = pd.DataFrame()
+    
+    return numbers_drawn, bingo_cards
+
+
+def part1():
+    
+    numbers_drawn, bingo_cards = parse()
+    
+    result = 0
+    for draw in numbers_drawn:
+        if result == 0:
+            for card_num, card in enumerate(bingo_cards):
+                    card = card.replace(draw, 'x')
+                    bingo_cards[card_num]= card
+                    if bingo(card):
+                        result = card.replace('x',0).to_numpy().sum() * draw
+
+    return result
+
+def part2():
+    
+    numbers_drawn, bingo_cards = parse()
+    
+    done = []
+    for draw in numbers_drawn:
+        for card_num, card in enumerate(bingo_cards):
+            if card_num not in done:
+                card = card.replace(draw, 'x')
+                bingo_cards[card_num]= card
+                if bingo(card):
+                    done = done + [card_num]
+                    result = card.replace('x',0).to_numpy().sum() * draw
+
+    return result
+
+print(part1())
+print(part2())
