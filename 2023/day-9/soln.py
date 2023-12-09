a5bb1ea874e6e0fcb04a90303f289b3c89b2f8ef
Josh Boddy <joshuaboddy1@live.co.uk>
2023-12-09 13:34:24 +0000

    better

--- 2023/day-9/soln.py
+++ 2023/day-9/soln.py
@@ -3,26 +3,18 @@ import re
 f = open("input.txt", "r")
 
 sum_of_next_values = 0
-part1 = True
+part1 = False
 
 for line in f.readlines():
-    
     history = [int(string_number) for string_number in re.findall(r'-?\d+', line)]
+   
     if not part1:
         history = history[::-1]
         
-    next_value = 0 
-    power = 0
-    
+    next_value = 0     
     while any([x != 0 for x in history]):
-        if part1:
-            next_value += history[-1] 
-            history = [history[i+1] - history[i] for i in range(len(history) - 1)]
-        else:
-            next_value += (-1)**power * history[-1]
-            history = [history[i] - history[i+1] for i in range(len(history) - 1)]
-
-        power += 1
+        next_value += history[-1] 
+        history = [history[i+1] - history[i] for i in range(len(history) - 1)]
 
     sum_of_next_values += next_value
     
